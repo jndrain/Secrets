e01400ae0971eadfdac6329c41c301f9b2a2cce6
Steven Huggard <Sven@MBP15.hsd1.wa.comcast.net>
2016-09-19 10:48:19 -0700

    steven's commits

--- apps/secrets/urls.py
+++ apps/secrets/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^secret$', views.secrets)
+    url(r'^$', views.index, name="index"),
+    url(r'^create_secret$', views.create_secret, name="create_secret")
 ]
--- secret_project/settings.py
+++ secret_project/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django_extensions',
     'apps.secrets',
     'django.contrib.admin',
     'django.contrib.auth',
